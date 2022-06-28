655ca378dfb0f1661ba6134a7c647f9e19f83118
Sumaiya Sinja <sumiyasinja@gmail.com>
2022-06-29 00:01:54 +0600

    Functionalized main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pyttsx3 as p
 import speech_recognition as sr
 import wiki_automation
 import youtube_automation
+import datetime
+import pyjokes
 
 engine = p.init()
 voices=engine.getProperty('voices')
@@ -12,56 +14,70 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-listener= sr.Recognizer()
-speak("Hello dear. How are you?")
 
-with sr.Microphone() as source:
-    listener.energy_threshold= 10000
-    listener.adjust_for_ambient_noise(source, 1.2)
-    print("Listining...")
-    audio= listener.listen(source)
-    try:
-        command= listener.recognize_google(audio)
-        print("You:\t"+ command)
-    except:
-        speak("Sorry Sir! I didn't get it.")
+speak("Good morning. How may I help you?")
 
-    if "what" and "about" and 'you' in command:
-        speak("I am Excellent.")
-
-    speak("How may I help you?")
-
-
-with sr.Microphone() as source:
-    listener.energy_threshold= 10000
-    listener.adjust_for_ambient_noise(source, 1.2)
-    print("Listining...")
-    audio = listener.listen(source)
-    text2 = listener.recognize_google(audio)
-    print("You:\t" + text2)
-
-    if "information" in text2:
-        speak("You want me to search what topic?")
-        with sr.Microphone() as source:
+def take_command():
+    listener = sr.Recognizer()
+    with sr.Microphone() as source:
             listener.energy_threshold = 10000
             listener.adjust_for_ambient_noise(source, 1.2)
             print("Listining...")
-            audio =listener.listen(source)
-            topic =listener.recognize_google(audio)
-            speak("Searching {} on wikipedia".format(topic))
-            assist = wiki_automation.wiki()
-            assist.get_info(topic)
+            audio= listener.listen(source)
+    try:
+        command = listener.recognize_google(audio)
+        print("You: " + command)
 
-    elif "youtube" or "play music" or "song" or "play video" in text2:
+    except:
+        speak("Warning:  Unable to reach u")
+
+    return (command)
+
+def runMe():
+    query= take_command()
+    if "time" in query:
+        time= datetime.datetime.now().strftime("%I:%M %p")
+        speak("The time is {} now".format(time))
+        print(time)
+
+    elif "information" in query:
+        speak("You want me to search what topic?")
+        topic = take_command()
+        speak("Searching {} on wikipedia".format(topic))
+        assist = wiki_automation.wiki()
+        assist.get_info(topic)
+
+    elif "youtube" or "play music" or "song" or "play video" in query:
         speak("Which video u want me to play")
-        with sr.Microphone() as source:
-            listener.energy_threshold = 1000
-            listener.adjust_for_ambient_noise(source, 1.2)
-            print("Listining...")
-            audio = listener.listen(source)
-            video = listener.recognize_google(audio)
-            speak("Playing {} on youtube".format(video))
-            assist = youtube_automation.YouTube()
-            assist.play_video(video)
+        video = take_command()
+        speak("Playing {} on youtube".format(video))
+        assist = youtube_automation.YouTube()
+        assist.play_video(video)
+
+    elif "joke" or "jokes" in query:
+        speak(pyjokes.get_joke())
+    else:
+        print("Sorry? Can you tell it again?")
+
+
+while 1:
+    runMe()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
